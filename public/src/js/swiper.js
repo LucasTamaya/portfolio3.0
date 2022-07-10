20d563a491df43d799224ca03109b6226fdcc628
LucasTamaya <lucastamaya.974@gmail.com>
2022-07-10 17:04:00 +0400

    partie responsive finit sauf la partie about

--- public/src/js/swiper.js
+++ public/src/js/swiper.js
@@ -1,15 +1,61 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: "#swiper-button-next",
-    prevEl: "#swiper-button-prev",
-  },
-});
+var swipper;
+
+window.onload = () => sliderUpdate();
+
+window.addEventListener("resize", () => sliderUpdate());
+
+const sliderUpdate = () => {
+  if (window.innerWidth <= 640) {
+    swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      slidesPerGroup: 1,
+      loop: false,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: "#swiper-button-next",
+        prevEl: "#swiper-button-prev",
+      },
+    });
+  }
+
+  if (window.innerWidth > 640 && window.innerWidth <= 1024) {
+    swiper = new Swiper(".mySwiper", {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      slidesPerGroup: 2,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: "#swiper-button-next",
+        prevEl: "#swiper-button-prev",
+      },
+    });
+  }
+
+  if (window.innerWidth > 1024) {
+    swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: "#swiper-button-next",
+        prevEl: "#swiper-button-prev",
+      },
+    });
+  }
+};
