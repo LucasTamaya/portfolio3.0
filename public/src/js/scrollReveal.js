521699e8d810ba3935a462263fecb946ee62a036
LucasTamaya <lucastamaya.974@gmail.com>
2022-07-11 22:45:25 +0400

    maj au niveau des animations au scroll

---
+++ public/src/js/scrollReveal.js
@@ -0,0 +1,20 @@
+ScrollReveal().reveal("#presentationSection", {
+  duration: 2000,
+  reset: true,
+  interval: 200,
+});
+ScrollReveal().reveal("#aboutSection", {
+  duration: 2000,
+  reset: true,
+  interval: 200,
+});
+ScrollReveal().reveal("#projectsSection", {
+  duration: 2000,
+  reset: true,
+  interval: 200,
+});
+ScrollReveal().reveal("#contactSection", {
+  duration: 2000,
+  reset: true,
+  interval: 200,
+});
