2a355f5004b7816e75a799de97aa08a841c1c192
LucasTamaya <lucastamaya.974@gmail.com>
2022-07-27 11:09:34 +0400

    ajout d'insta + hulu version mobile à la section projets

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,15 +1,27 @@
 const hamburgerMenu = document.getElementById("hamburgerMenu");
 const crossIcon = document.getElementById("crossIcon");
+const html = document.querySelector("html");
 const nav = document.querySelector("nav");
+const navLinksSm = [...document.querySelectorAll("li a")];
+const footerLegal = document.querySelector(".footer--legal");
 
 hamburgerMenu.addEventListener("click", openMenu);
 
 crossIcon.addEventListener("click", closeMenu);
 
+navLinksSm.forEach((link) => link.addEventListener("click", closeMenu));
+
 function openMenu() {
   nav.classList.add("enable");
+  html.classList.add("scrollDisable");
+  footerLegal.style.position = "initial";
 }
 
 function closeMenu() {
   nav.classList.remove("enable");
+  html.classList.remove("scrollDisable");
+  // le temps que le menu se referme avant de réapparaitre
+  setTimeout(() => {
+    footerLegal.style.position = "relative";
+  }, 100);
 }
---
+++ public/src/js/logoLoading.js
@@ -0,0 +1,20 @@
+// const logoContainer = document.querySelector(".logoContainer");
+// const logoContainerAfter = window.getComputedStyle(logoContainer, "::after");
+
+// const totalHeight = document.body.scrollHeight;
+// const windowHeight = window.innerHeight;
+// const pageBottom = totalHeight - windowHeight;
+
+// // console.log("total :", totalHeight);
+// // console.log("window :", windowHeight);
+// // console.log("atBottom: ", pageBottom);
+
+// window.addEventListener("scroll", () => {
+//   let scroll = this.scrollY;
+//   var percentage = (pageBottom - scroll) / pageBottom;
+//   console.log(percentage * 100);
+//   logoContainerAfter.setProperty(innerWidth, `${percentage}%`);
+//   if (scroll === pageBottom) {
+//     alert("at bottom");
+//   }
+// });
