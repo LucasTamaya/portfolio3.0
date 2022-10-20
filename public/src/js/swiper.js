50c8b885989a6145ceddca2ff0cd18a13984b0ca
LucasTamaya <lucastamaya.974@gmail.com>
2022-10-20 11:03:51 +0400

    Update swiper.js + projects description

--- public/src/js/swiper.js
+++ public/src/js/swiper.js
@@ -1,56 +1,29 @@
-let swiper;
-
-window.onload = () => sliderUpdate();
-
-window.addEventListener("resize", () => sliderUpdate());
-
-const sliderUpdate = () => {
-  // paramètre du slider pour le format mobile
-  if (window.innerWidth <= 640) {
-    swiper = new Swiper(".mySwiper", {
-      // paramètre le nombre de slide par vue
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-      // paramètre l'espace entre chaque slide
-      spaceBetween: 30,
-      // revient à la slide du début quand on est arrivé à la dernière slide
-      loop: true,
-      loopFillGroupWithBlank: true,
-      // permet de gérer la navigation du slider si au clique des éléments aved l'id swiper-button-next ou swiper-button-prev
-      navigation: {
-        nextEl: "#swiper-button-next",
-        prevEl: "#swiper-button-prev",
-      },
-    });
-  }
-
-  // paramètre du slider pour le format tablette
-  if (window.innerWidth > 640 && window.innerWidth <= 1024) {
-    swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".mySwiper", {
+  // paramètre le nombre de slide par vue
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  // paramètre l'espace entre chaque slide
+  spaceBetween: 30,
+  // revient à la slide du début quand on est arrivé à la dernière slide
+  loop: true,
+  loopFillGroupWithBlank: true,
+  // permet de gérer la navigation du slider si au clique des éléments aved l'id swiper-button-next ou swiper-button-prev
+  navigation: {
+    nextEl: "#swiper-button-next",
+    prevEl: "#swiper-button-prev",
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    640: {
       slidesPerView: 2,
-      spaceBetween: 30,
-      slidesPerGroup: 2,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      navigation: {
-        nextEl: "#swiper-button-next",
-        prevEl: "#swiper-button-prev",
-      },
-    });
-  }
-
-  // paramètre du slider pour le format ordinateur
-  if (window.innerWidth > 1024) {
-    swiper = new Swiper(".mySwiper", {
+    },
+    1024: {
       slidesPerView: 3,
-      spaceBetween: 30,
-      slidesPerGroup: 3,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      navigation: {
-        nextEl: "#swiper-button-next",
-        prevEl: "#swiper-button-prev",
-      },
-    });
-  }
-};
+    },
+  },
+  autoplay: {
+    delay: 3200,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+});
